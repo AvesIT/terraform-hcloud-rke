8a59ee93be0f4326b404fb66901414983772f46c
nivo <n.vogels@ave-sit.nl>
2020-10-26 10:59:14 +0100

    Bump rke to 1.1.0

--- versions.tf
+++ versions.tf
@@ -12,7 +12,7 @@ terraform {
     }
     rke = {
       source  = "rancher/rke"
-      version = "~> 1.0.0"
+      version = "~> 1.1.0"
     }
     template = {
       source  = "hashicorp/template"
